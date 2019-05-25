d93c3722732780c90f9255ff08235e26aa3bf7de
Oleg Per <poltava28704@gmail.com>
2019-05-25 09:49:15 +0300

    use app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
  */
 
 require('./bootstrap');
-
+require('./popup');
 window.Vue = require('vue');
 
 /**
---
+++ resources/js/popup.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".my-row").click(function(){
+        var product = jQuery.parseJSON($(this).attr("data-product"));
+        $(".modal-card-title").text(product.name);
+        $(".modal-card-body p:eq(0)").text('descr: ' + product.descr);
+        $(".modal-card-body p:eq(1) b").text(product.category.name);
+        $(".modal-card-body p:eq(2) b").text(product.arrayTags);
+        $(".modal").addClass("is-active");
+
+    });
+    $(".delete").click(function () {
+        $(".modal").removeClass("is-active");
+    });
+    $(".modal-background").click(function () {
+        $(".modal").removeClass("is-active");
+    });
+});
+
+
